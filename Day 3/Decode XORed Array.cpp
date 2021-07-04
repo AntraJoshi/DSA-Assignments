b7b92784a00632bf74d95a214e76be4edb38115a
AntraJoshi <57592174+AntraJoshi@users.noreply.github.com>
2021-07-05 01:52:53 +0530

    Add files via upload

---
+++ Day 3/Decode XORed Array.cpp
@@ -0,0 +1,9 @@
+vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> res;
+        res.push_back(first);
+        for(int i=0;i<encoded.size();i++){
+            first=first^encoded[i];
+            res.push_back(first);
+        }
+      return res;      
+    }
